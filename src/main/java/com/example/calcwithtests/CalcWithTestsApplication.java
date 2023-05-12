9b76fc57d54df345bc25fb1a0d56afc469038d76
МАРК <markpan07@mail.ru>
2023-05-12 12:19:32 +0300

    Initial commit

---
+++ src/main/java/com/example/calcwithtests/CalcWithTestsApplication.java
@@ -0,0 +1,13 @@
+package com.example.calcwithtests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalcWithTestsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalcWithTestsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/calcwithtests/CalcWithTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.calcwithtests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalcWithTestsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
